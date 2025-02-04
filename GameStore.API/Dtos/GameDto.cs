b313305fe1004f58a7967f597f993413ecff6e7f
Yutama <yutama.budiman@iconagency.com.au>
2025-02-04 18:09:21 +1100

    validation on the apis requests

--- GameStore.API/Dtos/GameDto.cs
+++ GameStore.API/Dtos/GameDto.cs
@@ -1,3 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.API.Dtos;
 
-public record class GameDto(int Id, string Name, string Genre, decimal Price, DateOnly ReleaseDate);
+public record class GameDto(
+    int Id,
+    [Required] [StringLength(50)] string Name,
+    [Required] [StringLength(20)] string Genre,
+    [Range(1, 100)] decimal Price,
+    DateOnly ReleaseDate
+);
--- GameStore.API/Endpoints/GamesEndpoints.cs
+++ GameStore.API/Endpoints/GamesEndpoints.cs
@@ -5,74 +5,98 @@ namespace GameStore.API.Endpoints;
 
 public static class GamesEndpoints
 {
-  const string GetGameEndPointName = "GetGameById";
+    const string GetGameEndPointName = "GetGameById";
 
-  private static readonly List<GameDto> games = [
-    new (1, "The Last of Us Part II", "Action-adventure", 59.99m, new DateOnly(2020, 6, 19)),
-    new (2, "Ghost of Tsushima", "Action-adventure", 59.99m, new DateOnly(2020, 7, 17)),
-    new (3, "Cyberpunk 2077", "Action role-playing", 59.99m, new DateOnly(2020, 12, 10)),
-    new (4, "Demon's Souls", "Action role-playing", 69.99m, new DateOnly(2020, 11, 12)),
-    new (5, "Demon Slayer: Kimetsu no Yaiba – Hinokami Keppūtan", "Fighting", 59.99m, new DateOnly(2021, 10, 15))
-  ];
+    private static readonly List<GameDto> games =
+    [
+        new(1, "The Last of Us Part II", "Action-adventure", 59.99m, new DateOnly(2020, 6, 19)),
+        new(2, "Ghost of Tsushima", "Action-adventure", 59.99m, new DateOnly(2020, 7, 17)),
+        new(3, "Cyberpunk 2077", "Action role-playing", 59.99m, new DateOnly(2020, 12, 10)),
+        new(4, "Demon's Souls", "Action role-playing", 69.99m, new DateOnly(2020, 11, 12)),
+        new(
+            5,
+            "Demon Slayer: Kimetsu no Yaiba – Hinokami Keppūtan",
+            "Fighting",
+            59.99m,
+            new DateOnly(2021, 10, 15)
+        ),
+    ];
 
-  public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app){
-
-    var group = app.MapGroup("games");
-    // GET /games
-    group.MapGet("/", () => games);
-
-    // GET /games/{id}
-    group.MapGet("/{id}", (int id) =>
+    public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
     {
-      GameDto? game = games.Find(game => game.Id == id);
+        var group = app.MapGroup("games").WithParameterValidation();
+        // GET /games
+        group.MapGet("/", () => games);
 
-      return game is null ? Results.NotFound() : Results.Ok(game);
+        // GET /games/{id}
+        group
+            .MapGet(
+                "/{id}",
+                (int id) =>
+                {
+                    GameDto? game = games.Find(game => game.Id == id);
 
-    })
-    .WithName(GetGameEndPointName);
+                    return game is null ? Results.NotFound() : Results.Ok(game);
+                }
+            )
+            .WithName(GetGameEndPointName);
 
+        // POST /games
+        group.MapPost(
+            "/",
+            (CreateGameDto newGame) =>
+            {
+                GameDto game = new(
+                    games.Count + 1,
+                    newGame.Name,
+                    newGame.Genre,
+                    newGame.Price,
+                    newGame.ReleaseDate
+                );
+                games.Add(game);
+                return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
+            }
+        );
 
-    // POST /games
-    group.MapPost("/", (CreateGameDto newGame) =>
-    {
-      GameDto game = new(games.Count + 1, newGame.Name, newGame.Genre, newGame.Price, newGame.ReleaseDate);
-      games.Add(game);
-      return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
-    });
+        // PUT /games/{id}
+        group.MapPut(
+            "/{id}",
+            (int id, UpdateGameDto updatedGame) =>
+            {
+                var index = games.FindIndex(game => game.Id == id);
 
-    // PUT /games/{id}
-    group.MapPut("/{id}", (int id, UpdateGameDto updatedGame) =>
-    {
-      var index = games.FindIndex(game => game.Id == id);
+                if (index == -1)
+                {
+                    return Results.NotFound();
+                }
 
-      if (index == -1)
-      {
-        return Results.NotFound();
-      }
+                games[index] = new GameDto(
+                    id,
+                    updatedGame.Name,
+                    updatedGame.Genre,
+                    updatedGame.Price,
+                    updatedGame.ReleaseDate
+                );
 
-      games[index] = new GameDto(
-        id,
-        updatedGame.Name,
-        updatedGame.Genre,
-        updatedGame.Price,
-        updatedGame.ReleaseDate
-      );
+                return Results.NoContent();
+            }
+        );
 
-      return Results.NoContent();
-    });
+        // DELETE /games/{id}
+        group.MapDelete(
+            "/{id}",
+            (int id) =>
+            {
+                var index = games.FindIndex(game => game.Id == id);
 
-    // DELETE /games/{id}
-    group.MapDelete("/{id}", (int id) =>
-    {
-      var index = games.FindIndex(game => game.Id == id);
+                games.RemoveAt(index);
 
-      games.RemoveAt(index);
+                return Results.NoContent();
+            }
+        );
 
-      return Results.NoContent();
-    });
+        // group.MapGet("/", () => "Hello World!");
 
-    // group.MapGet("/", () => "Hello World!");
-
-    return group;
-  }
+        return group;
+    }
 }
