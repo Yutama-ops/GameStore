5af6060ef21f0583478fa6205a887499926064e2
Yutama <yutama.budiman@iconagency.com.au>
2025-02-04 15:37:07 +1100

    delete request

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -16,18 +16,30 @@ List<GameDto> games = [
 // GET /games
 app.MapGet("/games", () => games);
 
+// GET /games/{id}
 app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndPointName);
 
+// POST /games
 app.MapPost("/games", (CreateGameDto newGame) => {
   GameDto game = new(games.Count + 1, newGame.Name, newGame.Genre, newGame.Price, newGame.ReleaseDate);
   games.Add(game);
   return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
 });
 
+// PUT /games/{id}
 app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) => {
   var index = games.FindIndex(game => game.Id == id);
 
   games[index] = new GameDto(id, updatedGame.Name, updatedGame.Genre, updatedGame.Price, updatedGame.ReleaseDate);
+
+  return Results.NoContent();
+});
+
+// DELETE /games/{id}
+app.MapDelete("/games/{id}", (int id) => {
+  var index = games.FindIndex(game => game.Id == id);
+
+  games.RemoveAt(index);
 
   return Results.NoContent();
 });
