5ab5f1c8c17542e5eb9f7741666a8c6ebb94cff5
Yutama <yutama.budiman@iconagency.com.au>
2025-02-04 21:50:59 +1100

    config

--- GameStore.API/obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
+++ GameStore.API/obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff333ac040e03e29b3b0c02986ea8fe4cd201136")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
