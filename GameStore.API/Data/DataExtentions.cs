ca620db329a47770628beef60b6a825a78e7233b
Yutama <yutama.budiman@iconagency.com.au>
2025-02-08 22:32:59 +1100

    Async await task

--- GameStore.API/Data/DataExtentions.cs
+++ GameStore.API/Data/DataExtentions.cs
@@ -5,11 +5,11 @@ namespace GameStore.API.Data;
 
 public static class DataExtentions
 {
-    public static void MigrateDb(this WebApplication app)
+    public static async Task MigrateDb(this WebApplication app)
     {
         using var scope = app.Services.CreateScope();
         var services = scope.ServiceProvider;
         var context = services.GetRequiredService<GameStoreContext>();
-        context.Database.Migrate();
+        await context.Database.MigrateAsync();
     }
 }
